38aca4eb52f3b59be11106fb359d7bf4880ab2f4
Óliver Becker <oliverbecker@usp.br>
2017-07-09 17:57:47 -0300

    Tentando dar merge...

--- Assets/Scripts/HPManager.cs
+++ Assets/Scripts/HPManager.cs
@@ -45,20 +45,20 @@ public class HPManager : MonoBehaviour {
 	void Die(){
 		GameObject.Destroy (gameObject);
 	}
-//	public float HP{
-//		get { return kHP; }
-//		set { kHP = value; }
-//	}
+/*	public float HP{
+		get { return kHP; }
+		set { kHP = value; }
+	}
 
-//	public bool decreaseHP (float value){
-//		kHP -= value;
-//
-//		if (kHP <= 0) {
-//			Debug.Log ("O hp chegou a 0");
-//			return false;
-//		}
-//
-//		return true;
-//	}
+	public bool decreaseHP (float value){
+		kHP -= value;
 
+		if (kHP <= 0) {
+			Debug.Log ("O hp chegou a 0");
+			return false;
+		}
+
+		return true;
+	}
+*/
 }
