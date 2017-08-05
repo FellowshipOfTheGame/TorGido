74b1681f48967e7d3299dc66f2012cf7a942a91d
Fer <fernandamarana44@gmail.com>
2017-08-05 10:36:29 -0300

    Menu Versao 1 pronto

---
+++ Assets/ClickSound.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine; 
+using System.Collections; 
+using UnityEngine.UI; 
+
+[RequireComponent(typeof(Button))] 
+public class ClickSound : MonoBehaviour  {     
+	public AudioClip sound;     
+	private Button button { get { return GetComponent<Button>(); } }    
+	private AudioSource source { get { return GetComponent<AudioSource>(); } } 
+
+
+	void Start () {
+		gameObject.AddComponent<AudioSource>();         
+		source.clip = sound;         
+		source.playOnAwake = false;         
+		button.onClick.AddListener(() => PlaySoud()); 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void PlaySoud (){         
+		source.PlayOneShot (sound);     
+	}
+}
