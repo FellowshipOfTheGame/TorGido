9c724fc8d04be82fd2977282d17641cb575eb0e9
HenriqueAnraki <henrique_anraki@outlook.com>
2017-08-05 17:29:47 -0300

    alterado para 4 waves para o boss

--- Assets/Scripts/SpawnController.cs
+++ Assets/Scripts/SpawnController.cs
@@ -16,7 +16,7 @@ public class SpawnController : MonoBehaviour {
 
 	//criar tempo de espera entre waves
 	private int wave = 1;
-	private int cycle = 2; //numeros de waves em um ciclo (normais + boss/semi-boss)
+	private int cycle = 5; //numeros de waves em um ciclo (normais + boss/semi-boss)
 	private int numEnemy = 5 - 1; //numero de inimigos na primeira wave do ciclo - 1
 
 	private Object newObject;
