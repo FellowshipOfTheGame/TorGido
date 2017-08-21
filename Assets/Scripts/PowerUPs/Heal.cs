fc56b6f0e4d8e26fed7f796b489cf654d3a79a42
Óliver Becker <oliverbecker@usp.br>
2017-08-21 20:36:51 -0300

    Agora o heal funciona mesmo!!

--- Assets/Scripts/PowerUPs/Heal.cs
+++ Assets/Scripts/PowerUPs/Heal.cs
@@ -7,14 +7,25 @@ public class Heal : PowerUP {
 	public int Increase_apple;
 	public int Increase_meat;
 	public Sprite[] sprite;
+	private GameObject gido;
 
-	private int rand;
 	private SpriteRenderer render;
+	private bool meat;			// True: heal mais forte (meat). False: heal mais fraco (apple)
+	private float chance;
 
 	// Use this for initialization
 	void Start () {
 		Initialization ();
 		render = gameObject.GetComponent<SpriteRenderer> ();
+		gido = GameObject.FindGameObjectWithTag ("Player");
+		chance = 25 + 50 * (1 - gido.GetComponent<HPManager> ().CurrentHealth / gido.GetComponent<HPManager> ().MaxHealth);
+		if (Random.Range (0, 100) <= chance) {
+			meat = true;
+			render.sprite = sprite [1];
+		} else {
+			meat = false;
+			render.sprite = sprite [0];
+		}
 	}
 	
 	// Update is called once per frame
@@ -24,8 +35,10 @@ public class Heal : PowerUP {
 
 	void OnTriggerEnter2D(Collider2D col) {
 		if (col.gameObject.tag == "Player") {
-			//if (
+			if (meat)
 				col.gameObject.GetComponent<HPManager> ().CurrentHealth += Increase_meat;
+			else
+				col.gameObject.GetComponent<HPManager> ().CurrentHealth += Increase_apple;
 			if (col.gameObject.GetComponent<HPManager> ().CurrentHealth > col.gameObject.GetComponent<HPManager> ().MaxHealth)
 				col.gameObject.GetComponent<HPManager> ().CurrentHealth = col.gameObject.GetComponent<HPManager> ().MaxHealth;
 			col.gameObject.GetComponent<HPManager> ().bar.value = col.gameObject.GetComponent<HPManager> ().CalculateHealth ();
