c1e2d9288c756b9f69168c4479b016adb747a008
HenriqueAnraki <henrique_anraki@outlook.com>
2017-08-21 16:53:38 -0300

    Criando arena inicial (sem paredes e spawners quebrados)

---
+++ Assets/Scripts/TilePosition.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TilePosition : MonoBehaviour {
+
+	private float k = 100f; //constante para posicionamento das tiles do background (profundidade)
+
+	// Use this for initialization
+	void Start () {
+		transform.position = new Vector3 (transform.position.x, transform.position.y, k + transform.position.y);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
