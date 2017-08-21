2f3dec015579ed51067acdfa57ab43c385f54133
Óliver Becker <oliverbecker@usp.br>
2017-08-21 20:04:39 -0300

    Heal mais forte(meat) e mais fraco(apple) funcionando

--- Assets/Scripts/PowerUPs/Heal.cs
+++ Assets/Scripts/PowerUPs/Heal.cs
@@ -4,11 +4,17 @@ using UnityEngine;
 
 public class Heal : PowerUP {
 
-	public int Increase;
+	public int Increase_apple;
+	public int Increase_meat;
+	public Sprite[] sprite;
+
+	private int rand;
+	private SpriteRenderer render;
 
 	// Use this for initialization
 	void Start () {
 		Initialization ();
+		render = gameObject.GetComponent<SpriteRenderer> ();
 	}
 	
 	// Update is called once per frame
@@ -18,7 +24,8 @@ public class Heal : PowerUP {
 
 	void OnTriggerEnter2D(Collider2D col) {
 		if (col.gameObject.tag == "Player") {
-			col.gameObject.GetComponent<HPManager> ().CurrentHealth += Increase;
+			//if (
+				col.gameObject.GetComponent<HPManager> ().CurrentHealth += Increase_meat;
 			if (col.gameObject.GetComponent<HPManager> ().CurrentHealth > col.gameObject.GetComponent<HPManager> ().MaxHealth)
 				col.gameObject.GetComponent<HPManager> ().CurrentHealth = col.gameObject.GetComponent<HPManager> ().MaxHealth;
 			col.gameObject.GetComponent<HPManager> ().bar.value = col.gameObject.GetComponent<HPManager> ().CalculateHealth ();
